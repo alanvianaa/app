9c3412197105a707bc007ad191993bf4301e73fc
alanvianaa <alanviana.nascimento@gmail.com>
2019-12-30 20:36:38 -0300

    first commit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import firebase from "firebase"
 
 Vue.config.productionTip = false
 
@@ -8,3 +9,16 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+var firebaseConfig = {
+  apiKey: "AIzaSyDgwGk9MWvHGSzYTEQmjv1m8HVromHF07E",
+  authDomain: "fazenda-api.firebaseapp.com",
+  databaseURL: "https://fazenda-api.firebaseio.com",
+  projectId: "fazenda-api",
+  storageBucket: "fazenda-api.appspot.com",
+  messagingSenderId: "359112140889",
+  appId: "1:359112140889:web:efccea9e63a334017c6948",
+  measurementId: "G-WGR1H2NPZJ"
+};
+
+firebase.initializeApp(firebaseConfig);
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
 
 Vue.use(VueRouter)
 
@@ -13,10 +15,17 @@ const routes = [
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/registrar',
+    name: 'signUp',
+    component: SignUp
   }
 ]
 
@@ -24,6 +33,6 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
-})
+});
 
 export default router
