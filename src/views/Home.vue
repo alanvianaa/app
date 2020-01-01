38faa333a573c4ad36d40370e511946cb88def55
alanvianaa <alanviana.nascimento@gmail.com>
2019-12-31 21:14:13 -0300

    Implementando o firebaseui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="home">
+    <!--
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    -->
+    <HelloWorld msg="Entrou no sistema"/>
     <button @click="logout">Sair</button>
     
   </div>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,56 +1,45 @@
 <template>
- <div class="container col-sm-3" >
-     <div id="firebaseui-auth-container"></div>
-<div class="align-self-center">
-    <div class="card" id="login_id">
-    <article class="card-body">
-        <a href="/registrar" class="float-right btn btn-outline-primary">Sign up</a>
-        <h4 class="card-title mb-4 mt-1">Login </h4>
-        <p>
-            <!--
-            <a href="" class="btn btn-block btn-outline-info"> <i class="fab fa-twitter"></i> Login via Twitter</a>
-            <a href="" class="btn btn-block btn-outline-primary"> <i class="fab fa-facebook-f"></i> Login via facebook</a>
-            -->
-            <a @click="loginGoogle" href="" class="btn btn-block btn-outline-danger"> <i class="fab fa-google"></i> Login via Google</a>
-        </p>
-        <hr>
-        
-        <div class="form-group">
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <span class="input-group-text"> <i class="fa fa-user"></i> </span>
-            </div>
-            <input v-model="email" class="form-control" placeholder="Email or login" type="email">
-        </div> <!-- input-group.// -->
-        </div> <!-- form-group// -->
-        <div class="form-group">
-        <div class="input-group">
-            <div class="input-group-prepend">
-                <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
-            </div>
-            <input v-model="senha" class="form-control" placeholder="******" type="password">
-        </div> <!-- input-group.// -->
-        </div> <!-- form-group// -->
-        <div class="form-group">
-        <button @click="login" class="btn btn-primary btn-block"> Login  </button>
-        </div> <!-- form-group// -->
-        <p class="text-center"><a href="#" class="btn">Esqueceu a senha?</a></p>
-        <Auth/>
-    </article>
-    </div> <!-- card.// -->
-	
-</div> 
-</div> 
+    <div class="container col-sm-3" >    
+        <div class="align-self-center">
+            <div class="card" id="login_id">
+            <article class="card-body">
+                <a href="/registrar" class="float-right btn btn-outline-primary">Cadastre-se</a>
+                <h4 class="card-title mb-4 mt-1">Fazer Login </h4>
+                <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"> <i class="fa fa-user"></i> </span>
+                    </div>
+                    <input v-model="email" class="form-control" placeholder="Email or login" type="email">
+                </div> <!-- input-group.// -->
+                </div> <!-- form-group// -->
+                <div class="form-group">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text"> <i class="fa fa-lock"></i> </span>
+                    </div>
+                    <input v-model="senha" class="form-control" placeholder="******" type="password">
+                </div> <!-- input-group.// -->
+                </div> <!-- form-group// -->
+                <div class="form-group">
+                <button @click="login" class="btn btn-primary btn-block"> Login  </button>
+                </div> <!-- form-group// -->
+                <p class="text-center"><a href="#" class="btn">Esqueceu a senha?</a></p>
+                <hr>
+                <div id="firebaseui-auth-container"></div>
+            </article>
+            </div> <!-- card.// -->
+        </div> 
+    </div> 
 </template>
 <script>
 import firebase from 'firebase';
-import Auth from '@/components/Auth.vue';
+//import Auth from '@/components/Auth.vue';
+import * as firebaseui from "firebaseui"
+import "../../node_modules/firebaseui/dist/firebaseui.css"
 
 export default {
     name: "login",
-    components: {
-        Auth
-    },
     data(){
         return{
             email: '',
@@ -60,26 +49,34 @@ export default {
     methods: {
         login: function(){
             firebase.auth().signInWithEmailAndPassword(this.email, this.senha).then(
-                (user) => {
+                () => {
                     this.$router.replace('home');
-                    alert('Logado com '+user);
                 },
                 (error) => {
                     alert('Não foi possivel logar. '+ error.message)
                 }
             );
-        },
-        loginGoogle: function(){
-            var provider = new firebase.auth.GoogleAuthProvider();
-            //var provider = new firebase.auth.FacebookAuthProvider();
-            firebase.auth().signInWithPopup(provider).then(
-                (result) => {
-                alert('Paasou: '+result.user)
-                },
-                (error) => {
-                    alert('Não foi possivel logar. '+ error.message)
-                });
         }
+    },
+    mounted() {
+        let uiConfig = {
+            signInOptions: [
+              {
+                provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+              },
+              {
+                provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+              }
+            ],
+            callbacks: {
+                signInSuccessWithAuthResult() {
+                    
+                    window.location.href = '/'
+                }
+            }
+        }
+        var ui = new firebaseui.auth.AuthUI(firebase.auth())
+        ui.start("#firebaseui-auth-container", uiConfig)
     }  
 };
 </script>
